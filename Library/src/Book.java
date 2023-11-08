c5a79a4c4d29ce49b2cf2d84e2b0830eb4b6e02f
DaveLRB <148245690+DaveLRB@users.noreply.github.com>
2023-11-08 10:30:09 +0000

    Update to all except cart

--- Library/src/Book.java
+++ Library/src/Book.java
@@ -4,51 +4,7 @@ public class Book {
     private String authorName;
     private int bookID;
     private int yearOfPublishing;
-    private int availableStock;
-
-    public Book() {
-
-    }
-
-    public String getBookName() {
-        return bookName;
-    }
-
-    public void setBookName(String bookName) {
-        this.bookName = bookName;
-    }
-
-    public String getPublisherName() {
-        return authorName;
-    }
-
-    public void setPublisherName(String publisherName) {
-        this.authorName = publisherName;
-    }
-
-    public int getBookID() {
-        return bookID;
-    }
-
-    public void setBookID(int bookID) {
-        this.bookID = bookID;
-    }
-
-    public int getYearOfPublishing() {
-        return yearOfPublishing;
-    }
-
-    public void setYearOfPublishing(int yearOfPublishing) {
-        this.yearOfPublishing = yearOfPublishing;
-    }
-
-    public int getAvailableStock() {
-        return availableStock;
-    }
-
-    public void setAvailableStock(int availableStock) {
-        this.availableStock = availableStock;
-    }
+    public int availableStock;
 
     public Book(String bookName, String authorName, int yearOfPublishing, int availableStock, int bookID) {
         this.bookName = bookName;
@@ -56,6 +12,32 @@ public class Book {
         this.yearOfPublishing = yearOfPublishing;
         this.availableStock = availableStock;
         this.bookID = bookID;
+    }
+
+    public void decreaseStock() {
+        if (availableStock > 0) {
+            availableStock--;
+        } else {
+            System.out.println("Sorry, the book is out of stock.");
+        }
+    }
+
+    public void increaseStock() {
+        availableStock++;
+    }
+
+    public String toString() {
+        return "\033[33m" + " Bookname: " + "\033[0m" + bookName + "\033[33m" + " Author : " + "\033[0m" + authorName +
+                "\033[33m" + " Published year : " + "\033[0m" + yearOfPublishing + "\033[33m" + " Stock : " + "\033[0m"
+                + availableStock + "\033[33m" + " Book ID : " + "\033[0m" + bookID;
+    }
+
+    public String getBookName() {
+        return this.bookName;
+    }
+
+    public int getAvailableStock() {
+        return this.availableStock;
     }
 
 
--- Library/src/User.java
+++ Library/src/User.java
@@ -1,45 +1,37 @@
 public class User {
 
-
-    private String nameID;
-    private String userName;
-    private int userAge;
+    public String userName;
+    public int userAge;
     private String userPassword;
+    public boolean isAdmin;
 
+    public User(String userName, int userAge, String userPassword, boolean isAdmin) {
+        this.userName = userName;
+        this.userAge = userAge;
+        this.userPassword = userPassword;
+        this.isAdmin = true;
+    }
 
-    public User(String userName, int userAge, String userPassword, String yourRole) {
+    public User(String userName, int userAge, String userPassword) {
         this.userName = userName;
         this.userAge = userAge;
         this.userPassword = userPassword;
 
+
+    }
+
+    public String toString(){
+        return "\033[33m" + " Username : " + "\033[0m" + userName + "\033[33m" + " Age : " + "\033[0m" + userAge
+                + "\033[33m" + " Password : " + "\033[0m" + userPassword;
     }
 
     public String getUserName() {
         return userName;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public int getUserAge() {
-        return userAge;
-    }
-
-    public void setUserAge(int userAge) {
-        this.userAge = userAge;
-    }
-
-
     public String getUserPassword() {
         return userPassword;
     }
-
-    public void setUserPassword(String userPassword) {
-        this.userPassword = userPassword;
-    }
-
-
 }
 
 
