198fcdee69fa57a8005f1a37860d5d9a040f95fd
DaveLRB <148245690+DaveLRB@users.noreply.github.com>
2023-11-08 15:35:48 +0000

    Update

--- FileToFile/src/Main.java
+++ FileToFile/src/Main.java
@@ -1,23 +1,40 @@
 import java.io.File;
-import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
-        File file = new File("PassData.txt");
-        FileWriter fileWriter = new FileWriter(file);
-        fileWriter.write("ola");
+        Scanner scanner = new Scanner("PassPass");
+        File file = new File("PassData");
+        FileWriter fileWriter = new FileWriter(file, true);
+        System.out.println("Write here");
+        scanner.nextLine();
+        fileWriter.write("BS");
         fileWriter.flush();
         fileWriter.close();
 
+        FileReader fileReader = new FileReader(file, StandardCharsets.UTF_8);
+        int i;
+        while((i = fileReader.read()) != -1) {
+            System.out.print((char) i);
+        }
+        fileReader.close();
 
+        File file1 = new File("PassPass");
 
+        FileReader fileReader1 = new FileReader(file1, StandardCharsets.UTF_8);
+
+        while ((i = fileReader1.read()) != -1){
+            System.out.print((char)i);
+        }
+        fileReader1.close();
 
     }
 
 
-
-
-    }
+}
